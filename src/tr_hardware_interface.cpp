023849bbb76769c8ddc91677baa96abf511726f3
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-03-06 06:19:02 +0100

    removed unnesccesairy command and state interfaces

--- src/tr_hardware_interface.cpp
+++ src/tr_hardware_interface.cpp
@@ -54,12 +54,6 @@ std::vector<hardware_interface::StateInterface> MercuryHardware::export_state_in
     state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
   }
 
-  ind = 0;
-  for (const auto & joint_name : joint_interfaces["acceleration"])
-  {
-    state_interfaces.emplace_back(joint_name, "acceleration", &joint_velocities_[ind++]);
-  }
-
   return state_interfaces;
 }
 
@@ -72,17 +66,6 @@ std::vector<hardware_interface::CommandInterface> MercuryHardware::export_comman
   {
     command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
   }
-
-  ind = 0;
-  for (const auto & joint_name : joint_command_interfaces["velocity"])
-  {
-    command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
-  }
-
-  ind = 0;
-  for (const auto & joint_name : joint_command_interfaces["acceleration"])
-  {
-    command_interfaces.emplace_back(joint_name, "acceleration", &joint_velocities_command_[ind++]);
   }
 
   return command_interfaces;
@@ -107,8 +90,6 @@ return_type MercuryHardware::write(const rclcpp::Time &, const rclcpp::Duration 
 {
   for (int i = 0; i < 6; i++) {
     double punning_position.f = joint_command_interfaces["position"][i] * 100;
-    double punning_velocity.f = joint_command_interfaces["velocity"][i] * 100;
-    double punning_acceleration.f = joint_command_interfaces["acceleration"][i] * 100;
 
     Hndl.SetInputPos(i, punning_position.u, punning_velocity.u, punning_acceleration.u);
   }
